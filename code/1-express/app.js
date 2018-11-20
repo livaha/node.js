d410b555ae8a2ff305337f00fed319c9582958eb
sbwxffnhc <1192137406@qq.com>
2018-11-21 03:34:26 +0800

    Rename
    目录加编号

--- code/1-express/app.js
+++ code/1-express/app.js
@@ -22,6 +22,15 @@ app.use('/public/',express.static('./public/'))
 app.use(express.static('./public/'))
 
 
+// Express 对于没有设定的请求路径，默认会返回 Cat not get xxx
+// 如果你想要定制这个 404
+// 需要通过中间件来配置
+// 只需要在自己的路由之后增加一个
+app.use(function (req, res) {
+  // 所有未处理的请求路径都会跑到这里
+  // 404
+})
+
 app.listen(8888,function(){
 	console.log('running...\nPelease use 127.0.0.1:8888 to access..')
 })
