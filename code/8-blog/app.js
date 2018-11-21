9d8eeda91ee01b4dc7de6b25485ba10704f2f1c3
sbwxffnhc <1192137406@qq.com>
2018-11-22 02:38:56 +0800

    配置POST和路由

--- code/8-blog/app.js
+++ code/8-blog/app.js
@@ -1,19 +1,23 @@
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
+var router = require('./router')
 
 var app = express()
 
 app.engine('html',require('express-art-template'))
 
 app.use('/public',express.static(path.join(__dirname,'./public/')))
-app.use('node_modules/',express.static(path.join(__dirname,'./node_modules')))
+app.use('/node_modules',express.static(path.join(__dirname,'../node_modules')))
 
 //默认会去项目中的 views 目录查找该模板文件
 app.set('views',path.join(__dirname,'./views/'))
 
-app.get('/',function(req,res){
-	res.send('hello')
-})
+/*配置body-parser*/
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(router)
 
 app.listen(8888,function(){
 	console.log('running ... 8888')
