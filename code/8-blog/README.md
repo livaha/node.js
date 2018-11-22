067098c71e9649cd88d0972afa934218bf9989d3
sbwxffnhc <1192137406@qq.com>
2018-11-22 14:06:32 +0800

    注册POST表单

--- code/8-blog/README.md
+++ code/8-blog/README.md
@@ -14,7 +14,44 @@
 
 #### 4.配置数据模型（数据库操作）
 
+  creat_time: {
+    type: Date,
+    default: Date.now      //这里没有直接加()是因为与了后会即刻调用； 这里只是给了一个方法，当new Model的时候，如果你没有传递creat_time，则mongoose就会调用default的值作为默认值
+  },
+
+
+
+#### 5.数据库操作
+
+或查询
+
+```
+ $or: [
+         {key1: value1}, {key2:value2}
+      ]
+```
 
 
 
 
+
+#### 6.ajax和状态码设置
+
+![1542861572609](assets/1542861572609.png)
+
+#### 7.MD5加密
+
+https://github.com/blueimp/JavaScript-MD5
+
+多层加密 
+
+
+
+#### 8.表单同步、异步提交
+
+      表单具有默认的提交行为，默认是同步的，同步表单提交，浏览器会锁死（转圈儿）等待服务端的响应结果。
+      
+        <form id="register_form" method="post" action="/register">
+      表单的同步提交之后，无论服务端响应的是什么，都会直接把响应的结果覆盖掉当前页面。
+    
+      后来有人想到了一种办法，来解决这个问题。
--- code/8-blog/router.js
+++ code/8-blog/router.js
@@ -1,4 +1,7 @@
 var express = require('express')
+var User = require('./models/user')
+
+var md5 = require('blueimp-md5')
 
 var router = express.Router()
 
@@ -20,7 +23,65 @@ router.get('/register',function(req,res){
 })
 
 router.post('/register',function(req,res){
+	/*1.获取表单注册提交过来的数据
+	  2.判断该用户是否存在
+	     如果存在，不允许注册
+	     如果不存在，注册新用户
+	  3.发送新的响应*/
 	console.log(req.body)
+	var body = req.body
+	//查邮箱是否重复,then判断昵称是否重复
+	User.findOne({
+		$or:[
+		{
+			email:body.email
+		},{
+			nickname:body.nickname
+		}]
+	},function(err,data){
+		if(err){
+			console.log('err')
+			//return res.status(500).send('Server Error')
+			return res.status(500).json({
+				success:false,
+				message:'server err'
+
+			})
+		}
+		if(data){
+			//return res.status(200).send('Email or Nickname aready exists')
+			return res.status(200).json({
+			err_code:1,
+			message:'Email or Nickname aready exists'
+
+			})
+		}
+
+		//对密码进行mdd5双重加密
+		body.password = md5(md5(body.password))
+
+		//保存记录
+		new User(body).save(function(err,user){
+			if(err){
+				return res.status(500).json({
+					err_code:500,
+					message:'Server busy,try later...'
+				})
+			}
+		})
+
+
+
+		console.log('ok')
+		//dataType: 'json' ajax中的这个是要求json格式 才会处理
+		//res.status(200),send('ok')
+		//express提供一个响应方法：json
+		res.status(200).json({
+			err_code:0,
+			message:'ok'
+		})
+
+	})
 })
 
 module.exports = router
